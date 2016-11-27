72738090288c31433001dfb251ad18d520337ad6
youngger9765 <purpleice9765@msn.com>
2016-11-27 16:09:59 +0800

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :repo_url, 'git@bitbucket.org:smartboss/hometaste.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/var/www/hometaste'
+set :deploy_to, '/home/deploy/hometaste'
 
 # Default value for :scm is :git
 # set :scm, :git
