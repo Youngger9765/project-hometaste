50fedfc187da272fecda097305761d0103b98f16
lululala <et3216@gmail.com>
2016-12-14 16:29:15 +0800

    fix mg db migration delete wrong migration

--- db/migrate/20161124024134_create_foods.rb
+++ db/migrate/20161124024134_create_foods.rb
@@ -1,9 +1,7 @@
 class CreateFoods < ActiveRecord::Migration[5.0]
   def change
     create_table :foods do |t|
-      t.integer :restaurant_id,null: false
       t.string :name,         null: false, default: ""
-      t.integer :price,       null: false, default: 0
 
       t.timestamps
     end
