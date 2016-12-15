57cca74f3d79c68c4838a120fd5301cf822e56c3
youngger9765 <purpleice9765@msn.com>
2016-12-15 13:37:38 +0800

    yml link add

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :deploy_to, '/home/deploy/hometaste'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/facebook.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/facebook.yml', 'config/google.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
