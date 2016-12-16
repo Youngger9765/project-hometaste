1b649b6b86803b3bc50e6a4fd7c4451a9fefc09d
youngger9765 <purpleice9765@msn.com>
2016-12-16 20:01:19 +0800

    if user is chef , no auth login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
-    if @user.persisted?
+    if !@user
+      flash[:alert] = "Your user account is chef, please login by email & password"
+      redirect_to root_path
+
+    elsif @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
@@ -15,7 +19,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
 
-    if @user.persisted?
+    if !@user
+      flash[:alert] = "Your user account is chef, please login by email & password"
+      redirect_to root_path
+
+    elsif @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
       sign_in_and_redirect @user, :event => :authentication
     else
