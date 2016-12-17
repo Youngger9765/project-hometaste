89ad4383db9a65bf23244325fe01723dd1e74330
youngger9765 <purpleice9765@msn.com>
2016-12-17 14:19:21 +0800

    check password before submit

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -7,6 +7,15 @@ class ChefsController < ApplicationController
 
 	def create
 		raise
+		@chef = Chef.new(chef_params)
+
+		if @chef.save
+			@restaurant = @chef.restaurant.build(restaurant_params)
+
+		else
+			render :action => :new
+		end
+
 	end
 
 	private
@@ -19,7 +28,15 @@ class ChefsController < ApplicationController
 	  	:restaurant_attributes => [
 	  		:id, :name, :address, :phone_number,:description,
 	  		:city, :state, :ZIP, :tax_ID, :communication_method,
-	  	]
+	  	],
+
+	  	:user_attributes => [
+	  		:id, :email, :password,
+	  	],
 	  )
+	end
+
+	def restaurant_params
+		chef_params[:restaurant_attributes]
 	end
 end
--- app/models/chef.rb
+++ app/models/chef.rb
@@ -6,4 +6,8 @@ class Chef < ApplicationRecord
 	accepts_nested_attributes_for :restaurant,
 		:allow_destroy => true,
 	  :reject_if => :all_blank
+
+	accepts_nested_attributes_for :user,
+		:allow_destroy => true,
+	  :reject_if => :all_blank
 end
