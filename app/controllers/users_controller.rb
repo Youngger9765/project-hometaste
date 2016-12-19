d610b99ec68a1ce4a6df618b1d47a0c95d412c1f
youngger9765 <purpleice9765@msn.com>
2016-12-19 23:12:58 +0800

    edit user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
-	before_action :find_user, :only =>[:edit]
-	before_action :is_current_user?, :only =>[:edit]
+	before_action :find_user, :only =>[:edit, :update]
+	before_action :is_current_user?, :only =>[:edit, :update]
 
 	def new
 		@user = User.new
@@ -11,7 +11,26 @@ class UsersController < ApplicationController
 		
 	end
 
+	def update
+		@user.update(user_params)
+
+		if @user.save
+			flash[:notice] = "Successfully edited"
+			redirect_to edit_user_path(@user)
+		else
+			flash[:alert] = "Fail edited..."
+			render :edit
+		end
+	end
+
 	private
+
+	def user_params
+	  params.require(:user).permit(
+	  	:name, :gender, :birthday, :phone_number, :address,
+	  	:ZIP, :foodie_id,
+	  )
+	end
 
 	def find_user
 		@user = User.find(params[:id])
