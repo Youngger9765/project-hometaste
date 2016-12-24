d1ded0a2d77fbecb3389e0dbb0b9fc5f3a9a0fd3
youngger9765 <purpleice9765@msn.com>
2016-12-24 22:03:48 +0800

    edit chef photo

--- app/helpers/chefs_helper.rb
+++ app/helpers/chefs_helper.rb
@@ -2,6 +2,7 @@ module ChefsHelper
 	def setup_chef(chef)
 	  chef.build_restaurant unless chef.restaurant
 	  chef.build_user unless chef.user
+	  chef.user.build_user_photo unless chef.user.user_photo
 	  chef.restaurant.build_delivery unless chef.restaurant.delivery
 	  2.times{chef.restaurant.bulk_buys.build}
 	  3.times{chef.restaurant.restaurant_cuisine_ships.build}
