7c471cf56b365fa1cb793adb81f14592bc132d1e
youngger9765 <purpleice9765@msn.com>
2016-12-24 15:03:18 +0800

    refactor

--- app/controllers/admin/main_controller.rb
+++ app/controllers/admin/main_controller.rb
@@ -6,21 +6,22 @@ class Admin::MainController < ApplicationController
 
 	def index
 
-		@restaurant_search = Restaurant.new
-		@user_search = User.new
-
 		if params[:restaurant]
 			@restaurant_search= Restaurant.new(:name => params[:restaurant][:name])
 			restaurant_ids = Restaurant.where('name LIKE ?', "%#{params[:restaurant][:name]}%").pluck(:id)
 			@q = Order.where(:restaurant_id => restaurant_ids).ransack(params[:q])
+
 		elsif params[:user]
 			@user_search = User.new(:foodie_id => params[:user][:foodie_id])
 			user_ids = User.where('foodie_id LIKE ?', "%#{params[:user][:foodie_id]}%").pluck(:id)
 			@q = Order.where(:user_id => user_ids).ransack(params[:q])
-		else
-			@q = Order.ransack(params[:q])
 
+		else
+			@restaurant_search = Restaurant.new
+			@user_search = User.new
+			@q = Order.ransack(params[:q])
 		end
+
     @orders = @q.result(distinct: true)
     @orders = @orders.page(params[:page]).per(5)
 
