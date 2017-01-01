2a052e1255d0a5f1a73dfe199c45e66b7dd1cd74
youngger9765 <purpleice9765@msn.com>
2017-01-01 23:30:11 +0800

    bigbuns view

--- app/models/big_bun.rb
+++ app/models/big_bun.rb
@@ -2,14 +2,15 @@ class BigBun < ApplicationRecord
 
 	before_create :set_code
 
-	belongs_to :restaurant
+	validates :start_datetime, :presence => true
+	validates :stop_datetime, :presence => true
 
+	belongs_to :restaurant
 	has_one :big_bun_photo
 
 	accepts_nested_attributes_for :big_bun_photo,
     :allow_destroy => true,
     :reject_if => :all_blank
-
 
 
 
