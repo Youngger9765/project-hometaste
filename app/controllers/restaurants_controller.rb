06e483a5f55ec2869dc5540ba38cc482e735124a
lululala <et3216@gmail.com>
2017-01-12 00:55:56 +0800

    fix restaurant big

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,9 +14,9 @@ class RestaurantsController < ApplicationController
     @today_orders = @chef.restaurant.orders.where(:created_at => time_range).where(:payment_status => "paid")
     @reach_percent = @today_orders ? (@today_orders.sum(:amount)*100/@restaurant.order_reach).to_f : 0
 
-    score_rate_i = @restaurant.food_avg_score.to_i
-    score_rate_f = ((@restaurant.food_avg_score - score_rate_i)/0.5).to_i
-    @score_rate = score_rate_i.to_s + ("_5" * score_rate_f)
+    # score_rate_i = @restaurant.food_avg_score.to_i
+    # score_rate_f = ((@restaurant.food_avg_score - score_rate_i)/0.5).to_i
+    # @score_rate = score_rate_i.to_s + ("_5" * score_rate_f)
   end
 
   private
