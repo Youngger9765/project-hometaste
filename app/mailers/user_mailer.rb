9c2fd363e67b33f4a9a44f968ece3c148b56c604
youngger9765 <purpleice9765@msn.com>
2017-01-23 22:28:03 +0800

    send mail not yet

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,14 @@
+class UserMailer < ApplicationMailer
+
+	default :from => "Homeaste <homeaste@homeaste.com>"
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.user_not_yet_order.subject
+  #
+  def user_not_yet_order(user, reason)
+    @user = user
+    @reason = reason
+    mail(:to => user.email, :subject => "[Homeaste] Refound Notify")
+  end
+end
