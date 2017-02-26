c90b898e2e5f5b1a76340df340444c0b7b13134b
lululala <et3216@gmail.com>
2017-02-26 22:50:40 +0800

    fix comment rated

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -4,8 +4,7 @@ module CommentsHelper
     if score.to_s[-1].to_i >= 5
       (score.to_s[0].to_i + 1).to_i
     elsif score.to_s[-1].to_i > 0
-      score.to_i + 0.5
-      score.to_s.gsub('.','_')
+      (score.to_i + 0.5).to_s.gsub('.','_')
     else
       score.to_i
     end
